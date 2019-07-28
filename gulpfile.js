a1578d4ad5e989af8b8665b7933423de2f66f019
jinnn0 <42154936+jinnn0@users.noreply.github.com>
2019-07-28 18:35:16 +0200

    initial upload

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      browserSync = require('browser-sync').create(),
+      uglifyCss = require('gulp-uglifycss')
+
+ 
+
+gulp.task('sass', function(){
+   return gulp.src('./app/src/styles/**/*.scss')
+              .pipe(sass()
+              .on('error', sass.logError))           
+              .pipe(gulp.dest('./app/temp/style'))
+              .pipe(browserSync.stream())
+    })
+
+
+gulp.task('scripts', function(){
+   return gulp.src('./app/src/scripts/*.js')
+              .pipe(gulp.dest('./app/temp/script'))
+})
+
+
+
+
+// watch all Sass files 
+// and run the sass task whenever a Sass file is saved
+gulp.task('watch', function(){
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: './app'
+    },
+  })
+
+  gulp.watch('./app/*.html', browserSync.reload)
+  gulp.watch('./app/src/styles/**/*.scss', ['sass'])
+  gulp.watch('./app/src/scripts/**/*.js', browserSync.reload)
+}) 
---
+++ src/scripts/app.js
@@ -0,0 +1,23 @@
+console.log("hello creative minds"); 
+function toggleMeu(){
+  let hamburger = document.querySelector('.hamburger')
+  let bar1 = hamburger.querySelector('.bar-1')
+  let bar2 = hamburger.querySelector('.bar-2')
+  let overlay = document.querySelector('.overlay')
+  let header = document.querySelector('.header')
+  let headerContent = header.querySelector('.header-content')
+  
+  hamburger.addEventListener('click', () => {
+    overlay.classList.toggle('overlay-visible')
+    header.classList.toggle('header-has-overlay')
+    headerContent.classList.toggle('header-content-has-overlay')
+    bar1.classList.toggle('bar-1-rotate')
+    bar2.classList.toggle('bar-2-rotate')
+    AOS.init()
+  })
+
+}
+toggleMeu()
+
+
+

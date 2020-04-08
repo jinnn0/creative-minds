1d5e91735a2b016b3abc6df97934a189287cc4e7
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-04-08 13:47:24 +0100

    fix

--- app/src/scripts/modules/toggleMenu.js
+++ app/src/scripts/modules/toggleMenu.js
@@ -1,31 +1,31 @@
 function toggleMeu(){ 
   let hamburger = document.querySelector('.hamburger')
-  let bar1 = hamburger.querySelectorAll('.bar-1')
-  let bar2 = hamburger.querySelectorAll('.bar-2')
-  let overlay = document.querySelectorAll('.overlay')
-  let overlayLeft = document.querySelectorAll('.js-overlay-left')
-  let overlayRight = document.querySelectorAll('.js-overlay-right')
-    
+  let bar1 = hamburger.querySelector('.bar-1')
+  let bar2 = hamburger.querySelector('.bar-2')
+  let overlay = document.querySelector('.overlay')
+  let overlayLeft = document.querySelector('.js-overlay-left')
+  let overlayRight = document.querySelector('.js-overlay-right')
+ 
   let click = 0 
-  hamburger.addEventListener('click', () => {
-    click++ 
-    if(click % 2 === 1) {
-      overlay.forEach(e => e.style.transition = "top 1s ease-in-out")
-      overlayLeft.forEach(e => e.classList.add("slide-in"))
-      overlayRight.forEach(e => e.classList.add("slide-in"))
-      overlayLeft.forEach(e => e.style.transition = "all .6s ease-in-out .7s")
-      overlayRight.forEach(e => e.style.transition = "all .6s ease-in-out .7s")
-    } else { 
-      overlay.forEach(e => e.style.transition = "top 1s ease-in-out .4s")
-      overlayLeft.forEach(e => e.classList.remove("slide-in"))
-      overlayRight.forEach(e => e.classList.remove("slide-in"))
-      overlayLeft.forEach(e => e.style.transition = "all .6s ease-in-out")
-      overlayRight.forEach(e => e.style.transition = "all .6s ease-in-out")
+  hamburger.addEventListener('click', (e) => {
+    click++   
+    if(click % 2 === 1) {    
+      overlay.style.transition = "top 1s ease-in-out"
+      overlayLeft.classList.add("slide-in") 
+      overlayRight.classList.add("slide-in")
+      overlayLeft.style.transition = "all .6s ease-in-out .7s"
+      overlayRight.style.transition = "all .6s ease-in-out .7s"
+    } else {  
+      overlay.style.transition = "top 1s ease-in-out .4s"
+      overlayLeft.classList.remove("slide-in")
+      overlayRight.classList.remove("slide-in")
+      overlayLeft.style.transition = "all .6s ease-in-out"
+      overlayRight.style.transition = "all .6s ease-in-out"
     }
 
-    overlay.forEach(e => e.classList.toggle('overlay-visible'))
-    bar1.forEach(e => e.classList.toggle('bar-1-rotate'))
-    bar2.forEach(e => e.classList.toggle('bar-2-rotate'))
+    overlay.classList.toggle('overlay-visible')
+    bar1.classList.toggle('bar-1-rotate')
+    bar2.classList.toggle('bar-2-rotate')
   })
 } 
    
